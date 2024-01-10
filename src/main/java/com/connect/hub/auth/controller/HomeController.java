a5fb11a7b1e3ce5bb13a432cd03bdaa00e6164b1
Ashwinn11 <ashwinnanbazhagan@gmail.com>
2024-01-10 23:34:59 +0530

    cors fix

--- src/main/java/com/connect/hub/auth/controller/HomeController.java
+++ src/main/java/com/connect/hub/auth/controller/HomeController.java
@@ -2,12 +2,14 @@ package com.connect.hub.auth.controller;
 
 import com.connect.hub.auth.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/home")
+@CrossOrigin
 public class HomeController {
     @Autowired
     private UserRepository userRepository;
--- src/main/java/com/connect/hub/profile/controller/ProfileController.java
+++ src/main/java/com/connect/hub/profile/controller/ProfileController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/profile")
+@CrossOrigin
 public class ProfileController {
 
     @Autowired
